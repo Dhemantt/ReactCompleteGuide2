1371b6d51a753609b919772441022f2beee51248
hemant Dixit <dixithemant02@gmail.com>
2022-10-19 16:58:15 +0530

    filtered by year

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
 const DUMMY_EXPENSES = [
-  // { id:1, title: "petrol", amount: 100, date: new Date(2022, 10, 31) },
-  // { id:2, title: "car insurance", amount: 1000, date: new Date(2022, 10, 30) },
-  // { id:3, title: "toilet paper", amount: 200, date: new Date(2022, 10, 29) },
-  // { id:4, title: "grocery", amount: 500, date: new Date(2022, 11, 31) },
+  { id:1, title: "petrol", amount: 100, date: new Date(2022, 10, 31) },
+  { id:2, title: "car insurance", amount: 1000, date: new Date(2022, 10, 30) },
+  { id:3, title: "toilet paper", amount: 200, date: new Date(2022, 10, 29) },
+  { id:4, title: "grocery", amount: 500, date: new Date(2022, 11, 31) },
 ];
 const App = () =>{
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,27 @@
+import React, { useState } from "react";
 import Card from "../Cards/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import Expenseitem from "./Expenseitem";
 import './Expenses.css';
 function Expenses(props){
-console.log(props)
-    const filterChangeHandler = () => {
 
+    const [filteredYear, setFilteredYear] = useState('2020')
+    const filterChangeHandler = () => {
+        setFilteredYear((selectedYear) => {
+            setFilteredYear(selectedYear);
+        })
     }
 
     return (
         <Card className="expenses">
             <ExpenseFilter 
-            
+            selected={filteredYear}            
             onChangeFilter={filterChangeHandler}/>
             {props.item.map( (x) => {
                 return  (
-                    <Expenseitem title={x.title} amount={x.amount} date={x.date} />
+                    <Expenseitem key={x.id} title={x.title} amount={x.amount} date={x.date} />
                 )   
             })}
-            {/* <Expenseitem title={props.title[0]} amount={props.amount[0].amount} date={props.date[0].date} />
-            <Expenseitem title={props.title[1]} amount={props.amount[1].amount} date={props.date[1].date} />
-            <Expenseitem title={props.title[2]} amount={props.amount[2].amount} date={props.date[2].date} />
-            <Expenseitem title={props.title[3]} amount={props.amount[3].amount} date={props.date[3].date} />    */}
         </Card>    
     );
 }
